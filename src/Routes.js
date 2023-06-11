a08621e2cdcb4d60c996f4163766013e7ca17979
Ivan Vieira <ivanvrjr@hotmail.com>
2023-06-11 12:27:59 -0300

    PerfilUsers
    Tentando adicionar o card com perfis.

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,15 @@ import Login from "./pages/Login";
 import { Routes as Rotas, Route } from "react-router-dom"
 import Cadastro from "./pages/Cadastro";
 import Termo from "./pages/Termo";
+import PerfilUser from "./pages/PerfilUsers";
 function Routes() {
     return (
         <Rotas>
             <Route path='/' element = {<Login/>} />
             <Route path="/cadastro" element={<Cadastro />} />
             <Route path="/termo" element={<Termo/>} />
-            </Rotas>
+            <Route path="/perfil" element={<PerfilUser/>} />
+        </Rotas>
     )
 }
 
