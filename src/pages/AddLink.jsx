172bf4838eb3745f4f3d74396b8ee4610a3e86d4
Elany Peixoto <elanysantos5@outlook.com>
2023-06-23 18:55:16 -0300

    Update AddLink.jsx

--- src/pages/AddLink.jsx
+++ src/pages/AddLink.jsx
@@ -1,146 +1,78 @@
-import {Button,
-    Form,
-    Input,
-  } from 'antd';
-import { API } from '../services/Api';
 import { useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { API } from '../services/Api';
+import { useParams } from 'react-router-dom';
   
-const formItemLayout = {
-  labelCol: {
-    xs: {
-      span: 24,
-    },
-    sm: {
-      span: 8,
-    },
-  },
-  wrapperCol: {
-    xs: {
-      span: 24,
-    },
-    sm: {
-      span: 16,
-    },
-  },
-};
-const tailFormItemLayout = {
-  wrapperCol: {
-    xs: {
-      span: 24,
-      offset: 0,
-    },
-    sm: {
-      span: 16,
-      offset: 8,
-    },
-  },
-};
-
-
 
 const AddLink = () => {
+    const [title, setTitle] = useState('');
+    const [url, setUrl] = useState('');
+    const [description, setDescription] = useState('');
+    const [userId, setUserId]= useState('');
+    const [imageUrl, setImageUrl]= useState('');
+    const [isPublic, setIsPublic]= useState('');
     const { id } = useParams();
-    const [user, setUser] = useState(null);
-    async function autenticar(values) {
-      try {
+  
+    const handleAddLink = async () => {
+        const dados= {title, url, description, userId, imageUrl, isPublic }
+      try { 
         const response = await API.post(`/users/${id}/links`,
-          values);
-    
-        localStorage.setItem("logado", true);
-    
-        setUser(response.data.user);
-    
-      } catch (error) {
-        alert(error.response.data.error)
-      }
-    }
-      const [form] = Form.useForm();
-      
-      
-      return (
-          <div id="formulario">
-              <h1>Adicionar Link</h1>
-              {user && <h2>Bem vindo, {user.name}</h2>}
-        <Form
-        method="POST" action=""
-          {...formItemLayout}
-          form={form}
-          name="register"
-          onFinish = {autenticar}
-          style={{
-            maxWidth: 600,
-          }}
-          scrollToFirstError
-        >
-          <Form.Item
-          name="title"
-          label="Nome do Link"
-          tooltip="Linkedin, GitHub or other..."
-          rules={[
-            {
-              required: true,
-              message: 'please enter the name of the link',
-              whitespace: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="description"
-          label="Nome do Link"
-          tooltip="Linkedin, GitHub or other..."
-          rules={[
-            {
-              required: true,
-              message: 'please enter the name of the link',
-              whitespace: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-          <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-    
-          <Form.Item
-            name="password"
-            label="Password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-            hasFeedback
-          >
-            <Input.Password />
-          </Form.Item>
-    
-          <Form.Item {...tailFormItemLayout}>
-            <Button type="primary">
-              Register
-            </Button>
-            Or <Link to="/login"> Login </Link>
-          </Form.Item>
-        </Form>
-        </div>
-      );
-}
+         dados );
 
+    localStorage.setItem("logado", true);
+        
+
+  
+        if (response.ok) {
+          console.log('Link adicionado:', { title, url, description, userId, imageUrl, isPublic });
+        } else {
+          console.log('Erro ao adicionar o link.');
+        }
+      } catch (error) {
+        console.log('Erro de conexão com a API:', error);
+      }
+    };
+  
+    return (
+      <div>
+        <h2>Adicionar Link</h2>
+        <input
+          type="string"
+          placeholder="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="url"
+          placeholder="URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <textarea
+          placeholder="Descrição"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          type="userId"
+          placeholder="userId"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+        />
+        <input
+          type="imageUrl"
+          placeholder="imageUrl"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+          <input
+          type="checkbox"
+          placeholder="isPublic"
+          value={isPublic}
+          onChange={(e) => setIsPublic(e.target.value='true')}
+        />
+        <button onClick={handleAddLink}>Adicionar</button>
+      </div>
+    );
+  }
+  
 export default AddLink
