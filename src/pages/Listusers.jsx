144af352a7df7b5c03151dbaf65720224d2135f6
Elany Peixoto <elanysantos5@outlook.com>
2023-10-05 13:16:26 -0300

    adicionando a funcionalidade de pesquisa

--- src/pages/Listusers.jsx
+++ src/pages/Listusers.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 import axios from "axios";
+import { Input, Space } from "antd";
+
+const { Search } = Input;
 
 function ListUsers() {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     getUsers();
@@ -11,18 +16,45 @@ function ListUsers() {
 
   async function getUsers() {
     const response = await axios.get("https://usuarios.ronierlima.dev/users");
-
-    setUsers(response.data);
+    const sortedUsers = response.data.sort((a, b) =>
+      a.name.localeCompare(b.name, undefined, { sensitivity: 'base' })
+    );
+    setUsers(sortedUsers);
   }
+
+  const handleSearch = () => {
+    const filteredUsers = users.filter((user) =>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(filteredUsers);
+  };
 
   return (
     <>
-      <div id="formulario" >
+      <div id="formulario" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+        <div>
+          <Space direction="vertical" >
+            <Search
+              placeholder="Pesquisar usuários"
+              allowClear
+              enterButton="Pesquisar"
+              size="large"
+              onSearch={handleSearch}
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+
+          </Space>
+        </div>
         <hr />
-        <div className="menu" >
-          {users.map((user) => (
-            <Card key={user.id} user={user} />
-          ))}
+        <div className="menu">
+          {searchResults.length > 0
+            ? searchResults.map((user) => (
+              <Card key={user.id} user={user} />
+            ))
+            : users.map((user) => (
+              <Card key={user.id} user={user} />
+            ))}
         </div>
       </div>
     </>
@@ -30,3 +62,4 @@ function ListUsers() {
 }
 
 export default ListUsers;
+
